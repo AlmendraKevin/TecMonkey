e4eedf001890d23d165764b5107d9640d679734c
AlmendraKevin <kevin.almendra@uabc.edu.mx>
2020-05-27 05:21:16 -0700

    Initial commit

---
+++ src/main/java/com/uabc/edu/mx/tecmonkey/TecMonkeyApplication.java
@@ -0,0 +1,13 @@
+package com.uabc.edu.mx.tecmonkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecMonkeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TecMonkeyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uabc/edu/mx/tecmonkey/TecMonkeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uabc.edu.mx.tecmonkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecMonkeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
