6f60a388b6cbb266165b3059aaabb261dba90dc0
AlmendraKevin <kevin.almendra@uabc.edu.mx>
2020-06-01 13:53:23 -0700

    update

--- src/main/java/com/uabc/edu/mx/tecmonkey/TecMonkeyApplication.java
+++ src/main/java/com/uabc/edu/mx/tecmonkey/TecMonkeyApplication.java
@@ -1,12 +1,13 @@
 package com.uabc.edu.mx.tecmonkey;
 
+import com.uabc.edu.mx.tecmonkey.repository.UserRepository;
 import com.uabc.edu.mx.tecmonkey.repository.VentaRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackageClasses = VentaRepository.class)
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class TecMonkeyApplication {
 
     public static void main(String[] args) {
