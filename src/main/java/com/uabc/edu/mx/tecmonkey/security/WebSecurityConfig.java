ae0f5a6dc67792b79d2660c04edb6197703da77c
AlmendraKevin <kevin.almendra@uabc.edu.mx>
2020-06-05 15:46:22 -0700

    estado actual

--- src/main/java/com/uabc/edu/mx/tecmonkey/security/WebSecurityConfig.java
+++ src/main/java/com/uabc/edu/mx/tecmonkey/security/WebSecurityConfig.java
@@ -41,7 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected  void configure(HttpSecurity httpSecurity) throws Exception{
         httpSecurity.csrf().disable()
-                .authorizeRequests().antMatchers("/","/css/**","/js/**","/img/**","/usuario/nuevo","/authenticate","/shop").permitAll()
+                .authorizeRequests().antMatchers("/","/css/**","/js/**","/img/**","/usuario/nuevo","/authenticate","/shop","/contacto","/promociones").permitAll()
                 .anyRequest().authenticated().and()
                 .formLogin()
                 .loginPage("/login")
