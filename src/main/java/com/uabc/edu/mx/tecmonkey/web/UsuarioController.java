d85678f898229a1bf9593eae68a79e96983853d1
AlmendraKevin <kevin.almendra@uabc.edu.mx>
2020-06-05 17:54:51 -0700

    update

--- src/main/java/com/uabc/edu/mx/tecmonkey/web/UsuarioController.java
+++ src/main/java/com/uabc/edu/mx/tecmonkey/web/UsuarioController.java
@@ -1,14 +1,33 @@
 package com.uabc.edu.mx.tecmonkey.web;
 
+import com.uabc.edu.mx.tecmonkey.model.User;
+import com.uabc.edu.mx.tecmonkey.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioController {
+    @Autowired
+    UsuarioService usuarioService;
+
+    @RequestMapping
+    public String getUsuarios(Model model){
+        List<User> user = usuarioService.getUsuarios() ;
+        model.addAttribute("user",user);
+        return "ControlUsuariosAdmin";
+    }
+    @RequestMapping("/new")
+    public String guardarRegistro(){
+        return "FormUsuarioAdmin";
+    }
 
     @RequestMapping("/nuevo")
-    public String home(){
+    public String crearNuevo(){
         return "RegistroUsuario";
     }
 
